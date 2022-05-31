89f85f6b583ff794a8b6cf1e0525bdfc06c4d03c
Charles <2275572017@qq.com>
2022-05-31 08:28:46 +0800

    多行字符；字体颜色设置；

---
+++ Locales/zhCN.lua
@@ -0,0 +1,18 @@
+local _, ns = ...
+local _, _, L = unpack(ns)
+if GetLocale() ~= "zhCN" then return end
+
+L["SlashWMessage"] = [=[
+听风的插件工具包，解决各插件版本问题，加载我即可解决。
+
+关注我：
+公众号 |cff00FA9A【听风轻咛】|r
+知乎 |cff00FA9A【听风轻咛】|r
+抖音 |cff00FA9A【WoWCube】|r
+	——我在听风，也在等你。
+]=]
+
+L['LoadAddonLog'] = [=[
+听风工具包已加载:
+输入命令/n查看更多帮助...
+]=]
