cd85b72dd69ccbecc6ae07e982f913a11d8e7d43
Charles <2275572017@qq.com>
2023-12-11 00:17:34 +0800

    Update zhCN.lua

--- Locales/zhCN.lua
+++ Locales/zhCN.lua
@@ -3,16 +3,18 @@ local _, _, L = unpack(ns)
 if GetLocale() ~= "zhCN" then return end
 
 L["SlashWMessage"] = [=[
-听风的插件工具包，解决各插件版本问题，加载我即可解决。
+听风的插件工具包。
 
 关注我：
-公众号 |cff00FA9A【听风轻咛】|r
-知乎 |cff00FA9A【听风轻咛】|r
-抖音 |cff00FA9A【WoWCube】|r
-	——我在听风，也在等你。
+|cff00FA9A【WoWCube】|r Bilibili
+|cff00FA9A【WowCube】|r 公众号
+|cff00FA9A【WoWCube】|r 抖音
+|cff00FA9A【听风轻咛】|r 知乎
+
+		——我在听风，也在等你。
 ]=]
 
 L['LoadAddonLog'] = [=[
 听风工具包已加载:
-输入命令/n查看更多帮助...
+输入命令/w查看更多帮助...
 ]=]
